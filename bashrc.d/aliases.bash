d0d51142706d968ee2d270f6c4d380b6c2ee20b6
dvandoor <dvandoor AT cisco Dot com>
2015-07-03 07:52:58 -0400

    added section in aliases for O/S specific commands

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -3,7 +3,6 @@ alias hex='printf "%x\n"'
 alias secpass='openssl rand -base64 14'
 alias hexpass='openssl rand -hex 10'
 alias du='du -h'
-alias dum='du -h -d 1'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
@@ -12,4 +11,15 @@ if [ "$(uname)" == "Darwin" ]; then
     # Mac OSX specific aliases
     alias ejectall="osascript -e 'tell application \"Finder\" to eject (every disk whose ejectable is true)'"
     alias lastconnected='defaults read /Library/Preferences/SystemConfiguration/com.apple.airport.preferences |grep LastConnected -A 7'
+    alias dum='du -h -d 1'
+fi
+
+if [ -f "/etc/redhat-release" ]; then
+    # CentOS/Redhat specific aliases
+  alias dum='du -h --max-depth=1'
+fi
+
+if [ -f "/etc/debian_version" ]; then
+    # Ubuntu/Devian specific aliases
+    alias dum='du -h -d 1'
 fi
