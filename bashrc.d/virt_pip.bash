81dedccd15fb93cd3ca6054c2e66c93e8392457d
dvandoor <dvandoor AT cisco Dot com>
2015-10-30 07:57:14 -0400

    commented out export PIP_DOWNLOAD_CACHE since it's now the default and is depricated

--- bashrc.d/virt_pip.bash
+++ bashrc.d/virt_pip.bash
@@ -1,7 +1,7 @@
 # pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=true
 # cache pip-installed packages to avoid re-downloading
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+#export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache # commented out 20151029 as it's now depricated
 
 gpip(){
    PIP_REQUIRE_VIRTUALENV="" pip "$@"
