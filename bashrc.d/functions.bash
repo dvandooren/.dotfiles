032329432c004335c27b42cd815ea5f32ad697f0
dvandoor <dvandoor AT cisco Dot com>
2020-02-10 13:59:53 -0500

    additional [ to [[

--- bashrc.d/functions.bash
+++ bashrc.d/functions.bash
@@ -1,15 +1,15 @@
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$(uname)" == "Darwin" ]]; then
     # Mac OSX specific functions
     source "$HOME"/.bashrc.d/osx.functions
 fi
 
-if [ -f "/etc/redhat-release" ]; then
+if [[ -f "/etc/redhat-release" ]]; then
     # CentOS/Redhat specific functions
     source "$HOME"/.bashrc.d/linux.functions
     source "$HOME"/.bashrc.d/rhel.functions
 fi
 
-if [ -f "/etc/debian_version" ]; then
+if [[ -f "/etc/debian_version" ]]; then
     # Ubuntu/Devian specific functions
     source "$HOME"/.bashrc.d/linux.functions
     source "$HOME"/.bashrc.d/deb.functions
